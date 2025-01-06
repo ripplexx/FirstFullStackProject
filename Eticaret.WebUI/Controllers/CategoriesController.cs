6a665121a7ba0fe5fed3398d841e3ec0b2dfe765
Eray Sönmezer <eray.sonmezer@hotmail.com>
2025-01-06 14:15:31 +0300

    have a error

---
+++ Eticaret.WebUI/Controllers/CategoriesController.cs
@@ -0,0 +1,33 @@
+﻿using Eticaret.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eticaret.WebUI.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public CategoriesController(DatabaseContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> IndexAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories.Include(p=>p.Products)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+            
+        }
+    }
+}
